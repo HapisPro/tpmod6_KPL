e21d03b41cab4c563a6ced7111afafab4039a4bf
HapisPro <alkausarhapis@student.telkomuniversity.ac.id>
2025-03-22 21:11:21 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300069/Program.cs
+++ tpmodul6_103022300069/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using tpmodul6_103022300069;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("test");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - M. Hafizh Al Kautsar");
+        video.IncreasePlayCount(1000000);
+        video.PrintVideoDetails();
     }
 }
